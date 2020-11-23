8c732a14bddcffade24f664b1ed0873bb33fe282
Tino Bakota <tbakota3@gmail.com>
2020-11-23 05:21:15 +0100

    added filter and env

--- src/home/HomePage.vue
+++ src/home/HomePage.vue
@@ -1,16 +1,22 @@
 <template>
     <div>
         <div v-pin="{ test: true, test2: true }">home</div>
-        <div v-pin-global="{ test: true, test2: true }">home</div>
+        <div v-pin-global="{ test: true, test2: true }">
+            {{ 5 | currency }}
+        </div>
         <div>home</div>
     </div>
 </template>
 
 <script>
 import pinDirective from '../shared/pin-directive';
+import currencyFilter from '../shared/currency-filter';
 
 export default {
     name: 'HomePage',
+    filters: {
+        currency: currencyFilter
+    },
     directives: { pin: pinDirective },
     created: function() {
         this.$store.dispatch('tasks/getTasks');
